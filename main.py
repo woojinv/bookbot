437c7acba210f3569d3b479abd7bb613697520f9
woojinv <oh.woojin95@gmail.com>
2024-12-14 22:31:15 -0600

    print character counts by descending value

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-with open("./books/frankenstein.txt") as f:
-    file_contents = f.read()
+def main():
+    path = "books/frankenstein.txt"
+    with open(f"./{path}") as f:
+        file_contents = f.read()
+        num_words = count_words(file_contents)
+        char_counts = count_chars(file_contents)
+
+        print(f"--- Begin report of {path} ---")
+        print(f"{num_words} found in the document")
+        print()
+        sorted_char_counts = dict(
+            sorted(char_counts.items(), key=lambda item: item[1], reverse=True)
+        )
+        for char, count in sorted_char_counts.items():
+            if char.isalpha():
+                print(f"the {char} character was found {count} times")
+
+    print("--- End report ---")
+
+
+def count_words(file_contents):
     words = file_contents.split()
-    num_of_words = len(words)
-    print(num_of_words)
+    return len(words)
 
-    lower_case = file_contents.lower()
 
-    dictionary = {}
-
-    for letter in lower_case:
-        if letter in dictionary:
-            dictionary[letter] += 1
+def count_chars(file_contents):
+    char_counts = {}
+    for char in file_contents:
+        lower_char = char.lower()
+        if lower_char in char_counts:
+            char_counts[lower_char] += 1
         else:
-            dictionary[letter] = 1
+            char_counts[lower_char] = 1
 
-    del dictionary[" "]
+    return char_counts
 
-    print(dictionary)
+
+main()
